210a16be556c37f7aad70f419b576c101ab3c991
soeguet <osman_s@live.de>
2023-08-21 22:42:52 +0200

    fixed resizing of screen when too small

--- src/main/java/com/soeguet/Main.java
+++ src/main/java/com/soeguet/Main.java
@@ -1,22 +1,23 @@
 package com.soeguet;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.soeguet.design.frame.CustomFrameImpl;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(
-        () -> {
-          FlatDarculaLaf.setup();
-          JFrame frame = new CustomFrameImpl();
-          frame.pack();
-          frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-          frame.setLocationRelativeTo(null);
-          frame.setVisible(true);
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            FlatDarculaLaf.setup();
+            JFrame frame = new CustomFrameImpl();
+            frame.pack();
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
-  }
+    }
 }
--- src/main/java/com/soeguet/design/frame/CustomFrameImpl.java
+++ src/main/java/com/soeguet/design/frame/CustomFrameImpl.java
@@ -3,60 +3,52 @@ package com.soeguet.design.frame;
 import com.soeguet.design.register.daily.DailyCash;
 import com.soeguet.design.register.daily.DailyCashImpl;
 import com.soeguet.design.register.weekly.WeeklyCashUpImpl;
+
 import java.awt.event.ActionEvent;
-import javax.swing.SwingUtilities;
 
 public class CustomFrameImpl extends CustomFrame {
 
-  public CustomFrameImpl() {
-    super();
-  }
+    public CustomFrameImpl() {
 
-  @Override
-  protected void menuItemDailyModule(ActionEvent e) {
-    DailyCash dailyModule = new DailyCashImpl();
-    dailyModule.setBounds(
-        0, 0, dailyModule.getPreferredSize().width, dailyModule.getPreferredSize().height);
-    resizeFrameIfNotBigEnough(
-        (int) dailyModule.getPreferredSize().getWidth(),
-        (int) dailyModule.getPreferredSize().getHeight());
-    form_desktopPane.add(dailyModule);
-  }
-
-  @Override
-  protected void menuItemWeeklyModule(ActionEvent e) {
-    WeeklyCashUpImpl weeklyModule = new WeeklyCashUpImpl();
-    weeklyModule.setBounds(
-        0, 0, weeklyModule.getPreferredSize().width+100, weeklyModule.getPreferredSize().height);
-    resizeFrameIfNotBigEnough(
-        (int) weeklyModule.getPreferredSize().getWidth(),
-        (int) weeklyModule.getPreferredSize().getHeight());
-    form_desktopPane.add(weeklyModule);
-  }
-
-  @Override
-  protected void quitMenuItem(ActionEvent e) {
-    System.exit(0);
-  }
-
-  private void resizeFrameIfNotBigEnough(int width, int height) {
-    if (width > this.getWidth()) {
-      SwingUtilities.invokeLater(
-          () -> {
-            this.setSize((int) (width * 1.1), this.getHeight());
-            Thread.yield();
-            resizeFrameIfNotBigEnough(width, height);
-            setLocationRelativeTo(this);
-          });
+        super();
     }
 
-    if (height > this.getHeight()) {
-      SwingUtilities.invokeLater(
-          () -> {
-            this.setSize(this.getWidth(), (int) (height * 1.1));
-            resizeFrameIfNotBigEnough(width, height);
-            setLocationRelativeTo(this);
-          });
+    @Override
+    protected void menuItemDailyModule(ActionEvent e) {
+
+        DailyCash dailyModule = new DailyCashImpl();
+        dailyModule.setBounds(0, 0, dailyModule.getPreferredSize().width, dailyModule.getPreferredSize().height);
+        resizeFrameIfNotBigEnough((int) dailyModule.getPreferredSize().getWidth(), (int) dailyModule.getPreferredSize().getHeight());
+        form_desktopPane.add(dailyModule);
     }
-  }
+
+    @Override
+    protected void menuItemWeeklyModule(ActionEvent e) {
+
+        WeeklyCashUpImpl weeklyModule = new WeeklyCashUpImpl();
+        weeklyModule.setBounds(0, 0, weeklyModule.getPreferredSize().width + 100, weeklyModule.getPreferredSize().height);
+        resizeFrameIfNotBigEnough((int) weeklyModule.getPreferredSize().getWidth(), (int) weeklyModule.getPreferredSize().getHeight());
+        form_desktopPane.add(weeklyModule);
+    }
+
+    @Override
+    protected void quitMenuItem(ActionEvent e) {
+
+        System.exit(0);
+    }
+
+    private void resizeFrameIfNotBigEnough(int width, int height) {
+
+        if (width > this.getWidth()) {
+
+            this.setSize((int) (this.getWidth() * 1.05), this.getHeight());
+            resizeFrameIfNotBigEnough(width, height);
+
+        } else if (height > this.getHeight()) {
+
+            this.setSize(this.getWidth(), (int) (this.getHeight() * 1.1));
+            resizeFrameIfNotBigEnough(width, height);
+
+        }
+    }
 }
